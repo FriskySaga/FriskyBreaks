e1586427104043e07d7b08a82fad09b914d43b37
FriskySaga <friskysaga@gmail.com>
2020-09-16 23:28:15 -0400

    Stop timer

--- MainWindow.cxx
+++ MainWindow.cxx
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
   ui->setupUi(this);
 
   // Start countdown from 50 minutes
-  ui->timerLabel->setText("50:00");
+  ui->timerLabel->setText(time->toString("m:ss"));
   connect(timer, &QTimer::timeout, this, &MainWindow::updateCountdown);
   timer->start(1000);
 }
@@ -26,5 +26,9 @@ void MainWindow::updateCountdown()
 {
   *time = time->addSecs(-1);
   ui->timerLabel->setText(time->toString("m:ss"));
+  if (time->minute() == 0 && time->second() == 0 && time->hour() == 0)
+  {
+    timer->stop();
+  }
 }
 
